4a1c2f4b07f9be1559fefaf57d5326b8840bb856
Natnael Mulugeta <natnaelmu.07@gmail.com>
2019-06-08 15:39:26 +1100

    product type spinner handled

--- app/src/main/java/com/example/openmarket/ProductUploadFragment.kt
+++ app/src/main/java/com/example/openmarket/ProductUploadFragment.kt
@@ -7,37 +7,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.fragment_product_upload.*
+import kotlinx.android.synthetic.main.fragment_product_upload.view.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [ProductUploadFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [ProductUploadFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class ProductUploadFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
     private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var product_types = arrayOf("Electronics", "Cloth", "Car", "House")
+    private lateinit var type: Spinner
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,9 +29,27 @@ class ProductUploadFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product_upload, container, false)
 
-        val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, listOf("None", "Top", "Bottom"))
-        adapter.setDropDownViewResource(android.R.layout.ff)
-        typeSpinner.adapter = adapter
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        type = view.typeSpinner
+
+
+        val adapter = ArrayAdapter(activity, android.R.layout.simple_spinner_item, product_types)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        typeSpinner!!.adapter = adapter
+
+        type.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val item = adapter.getItem(position)
+            }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -70,39 +71,10 @@ class ProductUploadFragment : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
+
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductUploadFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductUploadFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
