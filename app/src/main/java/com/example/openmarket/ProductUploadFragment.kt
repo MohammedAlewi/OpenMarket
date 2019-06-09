73c5e8048d48dfe759d02361f8d50cdc5bef201a
Natnael Mulugeta <natnaelmu.07@gmail.com>
2019-06-10 10:48:23 +1100

    handled product imgage for upload

--- app/src/main/java/com/example/openmarket/ProductUploadFragment.kt
+++ app/src/main/java/com/example/openmarket/ProductUploadFragment.kt
@@ -1,14 +1,19 @@
 package com.example.openmarket
 
+import android.app.Activity
 import android.content.ClipDescription
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.example.openmarket.data.Product
@@ -29,6 +34,7 @@ class ProductUploadFragment : Fragment() {
     private lateinit var amount: EditText
     private lateinit var price: EditText
     private lateinit var uploadBtn: Button
+    private lateinit var imageView: ImageView
 
     private var listener: OnFragmentInteractionListener? = null
 
@@ -50,6 +56,7 @@ class ProductUploadFragment : Fragment() {
         amount = view.amountEdit
         price = view.priceEdit
         uploadBtn = view.uploadBtn
+        imageView = view.productImage
 
         uploadBtn.setOnClickListener {
             val product = readFeilds()
@@ -59,8 +66,49 @@ class ProductUploadFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_signupFragment_to_loginFragment, bundle)
         }
 
+        imageView.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ActivityCompat.checkSelfPermission(requireContext(),
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, ProductUploadFragment.PERMISSION_CODE)
+                }else{
+                    pickImage()
+                }
+            }else{
+                pickImage()
+            }
+        }
+
         return view
 
+    }
+
+    private fun pickImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, ProductUploadFragment.IMAGE_PICK_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            ProductUploadFragment.PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    pickImage()
+                }
+                else {
+                    Toast.makeText(activity, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == ProductUploadFragment.IMAGE_PICK_CODE) {
+            imageView.setImageURI(data?.data)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -121,6 +169,11 @@ class ProductUploadFragment : Fragment() {
             type = ""
         )
         return product
+    }
+
+    companion object {
+        private val IMAGE_PICK_CODE = 1000
+        private val PERMISSION_CODE = 1001
     }
 
 }
