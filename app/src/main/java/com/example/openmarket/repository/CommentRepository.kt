e7a3c80a26bdf20f386241daba45bc48dd7ef036
MohammedAlewi <rofyalewi@gmail.com>
2019-06-09 14:09:33 +0300

    api methods are integrated with the repository...

--- app/src/main/java/com/example/openmarket/repository/CommentRepository.kt
+++ app/src/main/java/com/example/openmarket/repository/CommentRepository.kt
@@ -1,37 +1,105 @@
 package com.example.openmarket.repository
 
+import android.content.Context
+import android.os.AsyncTask
 import androidx.annotation.WorkerThread
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
+import com.example.openmarket.MainActivity
+import com.example.openmarket.api.OpenMarketApiService
 import com.example.openmarket.data.Comment
 import com.example.openmarket.data.CommentDao
 import com.example.openmarket.data.ProductCommentDao
 import com.example.openmarket.data.ProductCommentJoin
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.*
+import kotlin.random.Random
 
-class CommentRepository(private val commentDao: CommentDao,private val productCommentDao: ProductCommentDao){
+class CommentRepository(private val commentDao: CommentDao,private val productCommentDao: ProductCommentDao,private val activity:MainActivity){
 
     @WorkerThread
     fun insertComment(comment: Comment,product_id:Long){
-        val id=commentDao.insertComment(comment)
-        productCommentDao.insertProductComment(ProductCommentJoin(comment_id = id,product_id = product_id))
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val id=OpenMarketApiService.getInstance().saveComment(comment,product_id).await().body() as Long
+                comment.id=id
+                commentDao.insertComment(comment)
+                productCommentDao.insertProductComment(ProductCommentJoin(comment_id = id,product_id = product_id))
+            }
+        }else{
+            val id= Random(Date().time).nextLong()
+            comment.id=id
+            commentDao.insertComment(comment)
+            productCommentDao.insertProductComment(ProductCommentJoin(comment_id = id,product_id = product_id))
+            with(activity.getSharedPreferences("unsaved_data_on_server",Context.MODE_PRIVATE).edit()){
+                putLong("comment_insert",id)
+                apply()
+            }
+        }
     }
 
 
     fun deleteComment(comment: Comment){
-        productCommentDao.removeAllRelationByCommentId(comment.id)
-        commentDao.deleteComment(comment)
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                OpenMarketApiService.getInstance().deleteComment(comment.id)
+                productCommentDao.removeAllRelationByCommentId(comment.id)
+                commentDao.deleteComment(comment)
+            }
+        }else{
+            productCommentDao.removeAllRelationByCommentId(comment.id)
+            commentDao.deleteComment(comment)
+            with(activity.getSharedPreferences("unsaved_data_on_server",Context.MODE_PRIVATE).edit()){
+                putLong("comment_delete",comment.id)
+                commit()
+            }
+        }
     }
 
     fun getCommentForProduct(product_id:Long):LiveData<List<Comment>>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val comments= OpenMarketApiService.getInstance().getCommentForProduct(product_id).await().body() as List<Comment>
+                commentDao.insertComments(comments)
+            }
+        }
         return productCommentDao.getCommentsForProduct(product_id)
+
     }
 
     fun getCommentByUsername(username:String):LiveData<List<Comment>>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val comments= OpenMarketApiService.getInstance().getCommentByUsername(username).await().body() as List<Comment>
+                commentDao.insertComments(comments)
+            }
+        }
         return commentDao.getCommentByUsername(username)
+
+
     }
 
-    fun updateComment(comment: Comment)=commentDao.updateComment(comment)
+    fun updateComment(comment: Comment){
+        if (activity.isConnected()) {
+            GlobalScope.launch(Dispatchers.IO) {
+                OpenMarketApiService.getInstance().updateComment(comment, comment.id)
+                commentDao.updateComment(comment)
+            }
+        }
+    }
 
     fun getCommentById(id:Long):LiveData<Comment>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val comments= OpenMarketApiService.getInstance().getCommentById(id).await().body() as Comment
+                commentDao.insertComment(comments)
+            }
+        }
         return commentDao.getCommentById(id)
     }
+
 }
+
--- app/src/main/java/com/example/openmarket/repository/ProductRepository.kt
+++ app/src/main/java/com/example/openmarket/repository/ProductRepository.kt
@@ -1,40 +1,100 @@
 package com.example.openmarket.repository
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import com.example.openmarket.MainActivity
+import com.example.openmarket.api.OpenMarketApiService
 import com.example.openmarket.data.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.random.Random
 
 class ProductRepository(private val productDao: ProductDao, private val productCommentDao: ProductCommentDao,
-                        private val userProductDao: UserProductDao,private val commentDao: CommentDao){
+                        private val userProductDao: UserProductDao,private val commentDao: CommentDao,private val activity: MainActivity
+){
 
     fun insertProduct(product: Product,user_id:Long){
-        var id=productDao.insertProduct(product)
-        userProductDao.insertUserProduct(UserProductJoin(product_id = id,user_id = user_id))
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val id= OpenMarketApiService.getInstance().saveProductWithId(product,user_id).await().body() as Long
+                product.id=id
+                productDao.insertProduct(product)
+                userProductDao.insertUserProduct(UserProductJoin(product_id = id,user_id = user_id))
+            }
+        }else{
+            val id= Random(Date().time).nextLong()
+            product.id=id
+            productDao.insertProduct(product)
+            userProductDao.insertUserProduct(UserProductJoin(product_id = id,user_id = user_id))
+            with(activity.getSharedPreferences("unsaved_data_on_server", Context.MODE_PRIVATE).edit()){
+                putLong("product_insert",id)
+                apply()
+            }
+        }
     }
 
     fun getProductId(id:Long):LiveData<Product>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val products= OpenMarketApiService.getInstance().getProductId(id).await().body() as Product
+                productDao.insertProduct(products)
+            }
+        }
         return productDao.getProductById(id)
     }
 
     fun getProductsByUsername(username:String):LiveData<List<Product>>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val products= OpenMarketApiService.getInstance().getCommentByUsername(username).await().body() as List<Product>
+                productDao.insertProducts(products)
+            }
+        }
         return productDao.getProductsByUsername(username)
     }
 
 
     fun updateProduct(product: Product){
-        productDao.updateProduct(product)
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO) {
+                OpenMarketApiService.getInstance().updateProduct(product,product.id)
+                productDao.updateProduct(product)
+            }
+        }
+
     }
 
     fun deleteProduct(product: Product){
-        productCommentDao.removeAllRelationByProductId(product_id = product.id)
-        userProductDao.removeAllRelationByProductId(product_id = product.id)
-        productDao.deleteComment(product)
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO) {
+                OpenMarketApiService.getInstance().deleteProduct(product.id)
+
+                productCommentDao.removeAllRelationByProductId(product_id = product.id)
+                userProductDao.removeAllRelationByProductId(product_id = product.id)
+                productDao.deleteComment(product)
+            }
+        }
     }
 
     fun getCommentForProduct(product_id:Long):LiveData<List<Comment>>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val comments= OpenMarketApiService.getInstance().getCommentForProduct(product_id).await().body() as List<Comment>
+                commentDao.insertComments(comments)
+            }
+        }
         return productCommentDao.getCommentsForProduct(product_id)
     }
 
     fun getProductsForUser(user_id: Long): LiveData<List<Product>>{
+        if (activity.isConnected()){
+            GlobalScope.launch(Dispatchers.IO){
+                val products= OpenMarketApiService.getInstance().getProductsForUser(user_id).await().body() as List<Product>
+                productDao.insertProducts(products)
+            }
+        }
         return userProductDao.getProductsForUser(user_id)
     }
 }
