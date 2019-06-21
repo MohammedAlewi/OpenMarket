c9197d5e54371ed4148bdca3a7ee22fd74df3fed
MohammedAlewi <rofyalewi@gmail.com>
2019-06-21 13:46:21 +0300

    user profile fragment error is fixed

---
+++ app/src/test/java/com/example/openmarket/viewmodel/CommentViewModelTest.kt
@@ -0,0 +1,38 @@
+package com.example.openmarket.viewmodel
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CommentViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun deleteComment() {
+    }
+
+    @Test
+    fun getCommentForProduct() {
+    }
+
+    @Test
+    fun getCommentByUsername() {
+    }
+
+    @Test
+    fun updateComment() {
+    }
+
+    @Test
+    fun getCommentById() {
+    }
+}
